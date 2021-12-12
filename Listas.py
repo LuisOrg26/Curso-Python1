46356fd03bc9bd8fd648872230a7ae5cfd60080c
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2021-12-11 21:53:09 -0600

    Created a Shop List

--- Listas.py
+++ Listas.py
@@ -1,3 +1,26 @@
-vocales = ["a","e","i","o","u"]
+Compras = []
+articulo = ""
+siguiente = ""
+Titulo = "Lista de la Compra"
+print("{}\n".format(Titulo)+"-"*len(Titulo))
 
-print("a" in vocales)
+while articulo != "Q":
+    siguiente = ""
+    articulo = str(input("¿Que desea Comprar?\n"
+                         "(Presione Q para salir)\n"))
+    if articulo == "Q":
+        break
+    while siguiente not in ["S","N"]:
+        siguiente = str(input("¿Seguro que quiere agregar {}?\nPresione (S/N)\n".format(articulo)))
+
+    if siguiente == "S":
+        print("{} agregado".format(articulo))
+        Compras.append(articulo)
+    elif siguiente == "N":
+        print("{} no agregado".format(articulo))
+
+print("La lista de compras es:\n")
+for i in range(len(Compras)):
+    print(Compras[i])
+
+
