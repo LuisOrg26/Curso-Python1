bd2036244baf9dc8dca181e4baa42ffeec824474
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2021-12-11 22:26:17 -0600

    For Cycle for String

--- For.py
+++ For.py
@@ -1,10 +1,19 @@
-Vocales = ["a","e","i","o","u"]
-frase = "Hola, hoy estoy aprendiendo Python"
-vocales_encontradas = 0
+Signo = [" ",",","."]
+frase = "Hola, hoy estoy aprendiendo Python. Feliz Domingo."
+espacios_encontrados = 0
+comas_encontradas = 0
+puntos_encontrados = 0
 
 for letra in frase:
-    if letra in Vocales:
-        vocales_encontradas += 1
+    if letra in Signo:
+        if letra == " ":
+            print("Se ha encontrado un espacio\n")
+            espacios_encontrados += 1
+        if letra == ",":
+            print("Se ha encontrado una coma\n")
+            comas_encontradas += 1
+        if letra == ".":
+            print("Se ha encontrado un punto\n")
+            puntos_encontrados += 1
 
-
-print(vocales_encontradas)
+print("Se ha encontrado un total de {} espacios, {} comas, {} puntos".format(espacios_encontrados,comas_encontradas,puntos_encontrados))
