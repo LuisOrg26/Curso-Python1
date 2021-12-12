9f3b47623237377165a613720d6025433793d2b5
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2021-12-11 22:53:03 -0600

    Multiply For Cycle

--- For.py
+++ For.py
@@ -1,10 +1,4 @@
-import string
 
-texto_del_usuario = input("Introduzca un texto:\n")
-mayus = 0
-
-for letra in texto_del_usuario:
-    if letra in string.ascii_uppercase:
-        mayus += 1
-
-print("Las mayusculas son: {}".format(mayus))
+numero = int(input("Seleccione un numero\n"))
+for number in range(10):
+    print("{} x {} = {}".format(numero,number+1,numero*(number+1)))
