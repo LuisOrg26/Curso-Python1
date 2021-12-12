764aa8b3bdad28f0087144829f49e24d15955023
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2021-12-12 09:31:53 -0600

    Minus and Mayor For

--- For.py
+++ For.py
@@ -1,4 +1,17 @@
-#Multiplicar
-numero = int(input("Seleccione un numero\n"))
-for number in range(10):
-    print("{} x {} = {}".format(numero,number+1,numero*(number+1)))
+numeros = []
+añadir = "S"
+
+
+numeros_introducidos = input("Introduzca los numero separados por comas: ")
+numeros = [int(i) for i in numeros_introducidos.split(",")]
+print(numeros)
+numero_mayor = numeros[0]
+numero_menor = numeros[0]
+for numero in numeros[1:]:
+    if numero_menor > numero:
+        numero_menor = numero
+    if numero_mayor < numero:
+        numero_mayor = numero
+
+print("El numero más pequeño es: {}\n"
+      "Y el numero más grande es: {}".format(numero_menor,numero_mayor))
