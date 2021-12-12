35deeb766a855a48c13ca704d57df5dbc3bf6a91
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2021-12-11 22:33:21 -0600

    For Corrections

--- For.py
+++ For.py
@@ -1,19 +1,16 @@
 Signo = [" ",",","."]
-frase = "Hola, hoy estoy aprendiendo Python. Feliz Domingo."
+
 espacios_encontrados = 0
 comas_encontradas = 0
 puntos_encontrados = 0
-
+frase = str(input("Introduzca una frase"))
 for letra in frase:
     if letra in Signo:
         if letra == " ":
-            print("Se ha encontrado un espacio\n")
             espacios_encontrados += 1
         if letra == ",":
-            print("Se ha encontrado una coma\n")
             comas_encontradas += 1
         if letra == ".":
-            print("Se ha encontrado un punto\n")
             puntos_encontrados += 1
 
 print("Se ha encontrado un total de {} espacios, {} comas, {} puntos".format(espacios_encontrados,comas_encontradas,puntos_encontrados))
