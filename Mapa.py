522c66144a22e90d73dc52c50c02ad9cbbf6e7bb
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2021-12-13 14:14:32 -0600

    Adding Objects and Randomize

--- Mapa.py
+++ Mapa.py
@@ -1,23 +1,36 @@
 import os
-
 import readchar
-
+import random
 POS_X = 0
 POS_Y = 1
 MAP_WIDTH = 20
 MAP_HEIGHT = 15
-
+map_objects = []
 my_position = [3,1]
+for i in range(10):
+    x = random.randint(1,19)
+    y = random.randint(1,14)
+    object = [x,y]
+    map_objects.append(object)
+
 while True:
     print("+"+ "-" * MAP_WIDTH*3 + "+")
     for coordinate_y in range(MAP_HEIGHT):
         print("|", end="")
         for coordinate_x in range(MAP_WIDTH):
-            if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
-                print(" @ ", end="")
 
-            else:
-                print("   ", end ="")
+            char_to_draw = " "
+            for map_object in map_objects:
+                if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
+                    char_to_draw = "*"
+                if map_object[POS_X] == my_position[POS_X] and map_object[POS_Y] == my_position[POS_Y]:
+                    map_object[POS_X] = -1
+                    map_object[POS_Y] = -1
+            if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
+                char_to_draw = "@"
+
+
+            print(" {} ".format(char_to_draw), end ="")
         print("|")
     print("+"+ "-" * MAP_WIDTH*3 + "+")
 
