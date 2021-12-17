78043a7b4f18fcd4a8d8fc2e17c746035117890c
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2021-12-16 19:36:36 -0600

    Even or Odd

--- Function3.py
+++ Function3.py
@@ -1,8 +1,10 @@
-def suma(list):
-    nuevo = 0
-    for i in list:
-        nuevo += i
-    return nuevo
+def par_o_impar(number):
+    par = number % 2
+    if par == 1:
+        impar = True
+    if par == 0:
+        impar = False
+    return impar
 
 if __name__ == "__main__":
-    print(suma([1,5,6,7,8]))
+    print("El numero es impar: {}".format(par_o_impar(4)))
