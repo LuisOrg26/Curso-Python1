77f5849fde903a1e3d28bf51e1c13d833cbe6483
LuisOrg26 <91204064+LuisOrg26@users.noreply.github.com>
2021-12-18 08:56:27 -0600

    Shop List

--- Function3.py
+++ Function3.py
@@ -1,17 +1,21 @@
 from random import randint
 
-def guess(word):
-    know = False
-    while know == False:
-        number = int(input("¿Cual es el numero?"))
-        if number == word:
-            print("Felicidades encontraste el numero")
-            know = True
+def check(list):
+    art = input("¿Cual es el producto que quiere añadir a la lista?")
+    if art in list:
+        print("Este producto ya existe")
+        continu = str(input("¿Quiere añadir otro producto? (S/N)\n"))
+        if continu == "S":
+            check(list)
         else:
-            print("Vuelvalo a intentar")
+            pass
+    else:
+        lista_de_compra.append(art)
+        print("se ha añadido exitosamente el producto")
 
 
 
 if __name__ == "__main__":
-    numero = randint(1,100)
-    guess(numero)
+    lista_de_compra = ["huevo","pollo","frijol"]
+    check(lista_de_compra)
+    print(lista_de_compra)
